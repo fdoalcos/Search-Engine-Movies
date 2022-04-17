b772845b06c4298452b7add55ad0f394492d90b8
fdoalcos <alcosfrancis@gmail.com>
2022-04-17 01:22:13 +0000

    tae

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
-from flask import Flask, redirect, render_template, request
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, jsonify, session
+from flask_session import Session
 
 app = Flask(__name__)
 
-@app.route("/", method=["POST"])
+app.config["TEMPLATES_AUTO_RELOADED"] = True
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = SQL("sqlite:///users.db")
+
+@app.route("/")
+def login():
+        return render_template("login2.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
